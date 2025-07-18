bbac52352863cfff01d6f0cde767571027d11de4
Saurav576 <sauravkumarmishra576@gmail.com>
2025-07-18 12:19:39 +0530

    commit

--- admin_panel/src/context/AdminContext.jsx
+++ admin_panel/src/context/AdminContext.jsx
@@ -12,7 +12,7 @@ const AdminContextProvider=(props)=>{
      const [dashData,setDashData]=useState(false)
 
 
-    const backendUrl=import.meta.env.VITE_BACKEND_URL
+    const backendUrl="https://prescripto2backend.onrender.com"
 
     const getAllDoctors=async()=>{
         try {
